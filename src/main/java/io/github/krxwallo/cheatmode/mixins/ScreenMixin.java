535f096732a9d0808070abfa31df7e1e4982c5ae
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-12-31 15:21:58 +0100

    fix arrow keys not working correctly in chat screen

--- src/main/java/io/github/krxwallo/cheatmode/mixins/ScreenMixin.java
+++ src/main/java/io/github/krxwallo/cheatmode/mixins/ScreenMixin.java
@@ -1,8 +1,11 @@
 package io.github.krxwallo.cheatmode.mixins;
 
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import io.github.krxwallo.cheatmode.hooks.ScreenHooks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.navigation.GuiNavigationPath;
+import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,5 +27,12 @@ public class ScreenMixin {
     @Inject(at = @At("TAIL"), method = "render")
     public void render(DrawContext drawContext, int mouseX, int mouseY, float f, CallbackInfo ci) {
         ScreenHooks.INSTANCE.onScreenDraw((Screen) (Object) this, drawContext, mouseX, mouseY);
+    }
+
+    @WrapWithCondition(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;switchFocus(Lnet/minecraft/client/gui/navigation/GuiNavigationPath;)V"), method = "keyPressed")
+    private boolean switchFocus(Screen instance, GuiNavigationPath guiNavigationPath) {
+        // Fix arrow keys not working correctly in chat screen
+        //noinspection ConstantValue
+        return !(((Object) this) instanceof ChatScreen);
     }
 }
